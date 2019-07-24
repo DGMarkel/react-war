af021266fd4601b8f9098e03c0071f3df1ddc499
github username <github email address>
2019-07-24 01:15:00 -0400

    update state, update deal method

--- src/containers/Game.jsx
+++ src/containers/Game.jsx
@@ -4,7 +4,7 @@ class Game extends Component {
   constructor() {
     super();
     this.state={
-      deck: [],
+      deck: Array.from({length: 52}, (v, k) => k + 1 ),
       playerOne: [],
       playerTwo: [],
     }
@@ -20,8 +20,9 @@ class Game extends Component {
 
   deal = () => {
     for(let i = 0; i <= 52; i++) {
+      console.log(i)
       i % 2 == 0
-      ? this.setState({ ...this.state, playerOne: [...this.state.playerOne, i]})
+      ? this.setState({ ...this.state, playerOne: this.state.playerOne.concat(i)})
       : this.setState({ ...this.state, playerTwo: [...this.state.playerTwo, i]})
     }
   }
@@ -32,6 +33,7 @@ class Game extends Component {
       <div className="game">
         <h1>New Game!</h1>
         <button onClick={()=>{this.shuffleDeck()}}>Shuffle!</button>
+        <button onClick={()=>{this.deal()}}>Deal Cards!</button>
       </div>
     )
   }
