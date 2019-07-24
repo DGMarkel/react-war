18cd7bbd11f8eae523d31e163c1f1766d78cc149
github username <github email address>
2019-07-24 00:32:13 -0400

    import component from react library, add state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state={
+      startGame: false
+    }
+  }
+}
 
 function App() {
   return (
