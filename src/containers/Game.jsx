583428d67b37fb3a03ace419041ab7e7e27dc2b6
github username <github email address>
2019-07-24 01:03:18 -0400

    add methods

--- src/containers/Game.jsx
+++ src/containers/Game.jsx
@@ -10,6 +10,20 @@ class Game extends Component {
     }
   }
 
+  shuffleDeck = () => {
+    this.setState(
+      {}
+    )
+  }
+
+  deal = () => {
+    for(let i = 0; i <= 52; i++) {
+      i % 2 == 0
+      ? this.setState({ ...this.state, playerOne: [...this.state.playerOne, i]})
+      : this.setState({ ...this.state, playerTwo: [...this.state.playerTwo, i]})
+    }
+  }
+
   render() {
     return (
       <div className="game">
