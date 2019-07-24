051424ffcaa8a957988212914af050ced8f3d3ee
github username <github email address>
2019-07-24 00:44:22 -0400

    preliminary build of game container

---
+++ src/containers/Game.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Game extends Component {
+
+  render() {
+    return (
+      <div className="game">
+        <h1>New Game!</h1>
+      </div>
+    )
+  }
+}
+
+export default Game
