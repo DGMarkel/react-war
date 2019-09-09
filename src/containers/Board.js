ba0f42b96653b2c69074ae46cca3a23a2906937b
github username <github email address>
2019-09-08 23:23:46 -0400

    add notes

--- src/containers/Board.js
+++ src/containers/Board.js
@@ -4,6 +4,11 @@ class Board extends Component {
   render() {
     return (
       <div className="board">
+        // this is the "table" where games will be played
+        // <StatusMessages /> component will display messages like "player one wins," etc.
+        // <GameInPlay /> component will display cards being played
+        // <PlayerDeck /> component will display each deck and number of cards in deck
+        // <Menu /> component will display buttons like "Play game" "Play again" etc
       </div>
     )
   }
