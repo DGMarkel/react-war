93f3b9c6337c3466e6730af072d01f78e3971e45
github username <github email address>
2019-09-08 23:28:39 -0400

    import components to Board container

--- src/containers/Board.js
+++ src/containers/Board.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
+import Menu from '../components/Menu'
+import StatusMessages from '../components/StatusMessages'
 
 export default class Board extends Component {
   render() {
     return (
       <div className="board">
         // this is the "table" where games will be played
-        // <StatusMessages /> component will display messages like "player one wins," etc.
+        <StatusMessages /> // component will display messages like "player one wins," etc.
         // <GameInPlay /> component will display cards being played
         // <PlayerDeck /> component will display each deck and number of cards in deck
-        // <Menu /> component will display buttons like "Play game" "Play again" etc
+        <Menu /> // component will display buttons like "Play game" "Play again" etc
       </div>
     )
   }
