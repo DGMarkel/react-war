21d2daac21b642d9ee53ce093da775075d672c1a
github username <github email address>
2019-09-16 23:25:01 -0400

    comment out current render method

--- src/containers/Board.js
+++ src/containers/Board.js
@@ -27,22 +27,23 @@ export default class Board extends Component {
 
 
 
-  render() {
-    return (
-      <div className="board">
-        {
-          // this is the "table" where games will be played
-        }
-        <StatusMessages />
-        { // component will display messages like "player one wins," etc.
-          // <GameInPlay /> component will display cards being played
-          // <PlayerDeck /> component will display each deck and number of cards in deck
-        }
-        <Menu />
-        {
-          // component will display buttons like "Play game" "Play again" etc
-        }
-      </div>
-    )
-  }
+  // render() {
+  //   return (
+  //     <div className="board">
+  //       {
+  //         // this is the "table" where games will be played
+  //       }
+  //       <StatusMessages />
+  //       { // component will display messages like "player one wins," etc.
+  //         // <GameInPlay /> component will display cards being played
+  //         // <PlayerDeck /> component will display each deck and number of cards in deck
+  //       }
+  //       <Menu />
+  //       {
+  //         // component will display buttons like "Play game" "Play again" etc
+  //       }
+  //     </div>
+  //   )
+  // }
+
 }
