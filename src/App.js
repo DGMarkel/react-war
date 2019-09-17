10595ad3753fcb65eca2c315e17c17c7b70d6cfa
github username <github email address>
2019-09-16 23:19:37 -0400

    JXS-ify comments in container

--- src/App.js
+++ src/App.js
@@ -9,9 +9,8 @@ class App extends Component {
       <div className="board">
         <Board />
       </div>
-    )
+    );
   }
-
 
 }
 
--- src/containers/Board.js
+++ src/containers/Board.js
@@ -8,7 +8,7 @@ export default class Board extends Component {
   constructor() {
     super();
     this.state={
-      gameType: none
+      gameType: 'none'
     }
   }
 
@@ -17,11 +17,18 @@ export default class Board extends Component {
   render() {
     return (
       <div className="board">
-        // this is the "table" where games will be played
-        <StatusMessages /> // component will display messages like "player one wins," etc.
-        // <GameInPlay /> component will display cards being played
-        // <PlayerDeck /> component will display each deck and number of cards in deck
-        <Menu /> // component will display buttons like "Play game" "Play again" etc
+        {
+          // this is the "table" where games will be played
+        }
+        <StatusMessages />
+        { // component will display messages like "player one wins," etc.
+          // <GameInPlay /> component will display cards being played
+          // <PlayerDeck /> component will display each deck and number of cards in deck
+        }
+        <Menu />
+        {
+          // component will display buttons like "Play game" "Play again" etc 
+        }
       </div>
     )
   }
